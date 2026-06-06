0d942ea229f6456a179fb2acc659ebdcfe9929a1
Souvik Sinhababu <souviksinhababu1@gmail.com>
2026-06-06 15:56:28 +0530

    bug fix

--- Frontend/sw.js
+++ Frontend/sw.js
@@ -44,7 +44,7 @@ self.addEventListener('fetch', (e) => {
       return fetch(e.request);
     }).catch(() => {
       if (e.request.mode === 'navigate') {
-        return caches.match('/index.html');
+        return caches.match('/');
       }
     })
   );
